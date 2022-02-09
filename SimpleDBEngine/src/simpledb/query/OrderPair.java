049adb877334e870456fbc683112aadd8d95718b
Nauman Sajid <e0389102@u.nus.edu>
2022-02-09 20:55:14 +0800

    Project Assignment 3: Sorting

---
+++ SimpleDBEngine/src/simpledb/query/OrderPair.java
@@ -0,0 +1,27 @@
+package simpledb.query;
+
+public class OrderPair {
+	private String field;
+	private boolean isAsc;
+	
+	public OrderPair (String field,boolean isAsc) {
+		this.field = field;
+		this.isAsc = isAsc;
+	}
+	
+	public OrderPair (String field) {
+		this.field = field;
+		this.isAsc = true;
+	}
+	
+	public String getField() {
+		return field;
+	}
+	public boolean isAsc() {
+		return isAsc;
+	}
+	
+	public String toString() {
+		return String.format("%s %s", field,isAsc);
+	}
+}
