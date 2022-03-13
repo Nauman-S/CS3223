1384f619e15bf96f520c14f6ba5de0fa682fd846
Nauman Sajid <e0389102@u.nus.edu>
2022-03-13 16:27:37 +0800

    lab 6 DISTINCT PLAN
    completed development

--- SimpleDBEngine/src/simpledb/parse/Lexer.java
+++ SimpleDBEngine/src/simpledb/parse/Lexer.java
@@ -257,7 +257,7 @@ public class Lexer {
 	private void initKeywords() {
 		keywords = Arrays.asList("select", "from", "where", "and", "insert", "into", "values", "delete", "update",
 				"set", "create", "table", "int", "varchar", "view", "as", "index", "on", "order", "by", "group",
-				"explain");
+				"explain", "distinct");
 	}
 
 	private void initOperators() {
